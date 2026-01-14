6ef9c76edd041f4851579564561e27446de0b9bb
Christian Garland <239896361+christiangarland01-a11y@users.noreply.github.com>
2026-01-13 18:12:19 -0600

    Add functionality to calculator buttons
    Added functionality to all buttons other than decimal.
    Functions in place to allow calculations.

--- javascript.js
+++ javascript.js
@@ -1,15 +1,37 @@
 const screen = document.querySelector("#screen");
 const screenBtns = document.querySelectorAll(".screenBtn");
+const mathBtns = document.querySelectorAll(".mathBtn");
+const equalBtn = document.querySelector("#equalBtn");
+const decimalBtn = document.querySelector("#decimalBtn");
+const delBtn = document.querySelector("#delBtn");
 
-const operations = {
+const equations = {
     "+": add = (x, y) => x + y,
     "-": subtract = (x, y) => x - y,
     "*": multiply = (x, y) => x * y,
     "/": divide = (x, y) => x / y,
 }
 
+const operate = (x, mathSym, y) => {
+    return equations[mathSym](x, y);
+}
+
+delBtn.addEventListener("click", () => screen.textContent = "");
+
 screenBtns.forEach(btn => {
     btn.addEventListener("click", () => {
         screen.textContent += btn.textContent;
     });
 });
+
+mathBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        screen.textContent += ` ${btn.textContent} `;
+    })
+})
+
+equalBtn.addEventListener("click", () => {
+    const equation = screen.textContent.split(" ");
+    const answer = operate(parseInt(equation[0]), equation[1], parseInt(equation[2]));
+    screen.textContent = answer;
+})
